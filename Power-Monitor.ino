5142c8c272c11d1b8b34ea7885534c42cb7c26a5
Mingjie Li <limingjie@outlook.com>
2023-03-03 11:59:55 +0800

    Move to INA219

--- Power-Monitor.ino
+++ Power-Monitor.ino
@@ -1,61 +1,38 @@
 #include <Arduino.h>
-#include <Button.h>
 #include <INA.h>  // Zanshin INA Library
-#include <SPI.h>
-#include <TFT_eSPI.h>  // Hardware-specific library
 #include <Wire.h>
+#include <Arduino_GFX_Library.h>
+// #include <Button.h>
 
-#include "iosevka15.h"
-
-// https://github.com/Zanduino/INA/blob/master/examples/DisplayReadings/DisplayReadings.ino
+// The max shunt voltage of INA219 is +/- 320mv, with a 0.1 Ohm shunt resister, the max current is 0.32/0.1 = 3.2A.
+// The Zanduino/INA library automatically set the gain bases on shunt resister and max current, the code can be found in `INA_Class::initDevice()`
 const uint32_t SERIAL_SPEED    = 115200;  ///< Use fast serial speed
 const uint32_t SHUNT_MICRO_OHM = 100000;  ///< Shunt resistance in Micro-Ohm, e.g. 100000 is 0.1 Ohm
-const uint16_t MAXIMUM_AMPS    = 1;       ///< Max expected amps, clamped from 1A to a max of 1022A
+const uint16_t MAXIMUM_AMPS    = 3;       ///< Max expected amps, clamped from 1A to a max of 1022A
 uint8_t        devicesFound    = 0;       ///< Number of INAs found
 
 INA_Class INA;
-TFT_eSPI  tft = TFT_eSPI();
-Button    button(D6);  // GPIO12
-
-bool alert = false;
-
-void ICACHE_RAM_ATTR AlertCallback()
-{
-    // INA226 Alert PIN is Open Drain, so low means alert.
-    // Note: NodeMCU A0 is connected to a voltage divider, the ESP8266 ADC has a range of [0, 1.0] v.
-    alert = analogRead(A0) < 512;
-}
+Arduino_GFX *gfx;
+// Button    button(D6);  // GPIO12
 
 void setup()
 {
-    // put your setup code here, to run once:
     Serial.begin(SERIAL_SPEED);
 
-    // GPIO2
-    attachInterrupt(digitalPinToInterrupt(D4), AlertCallback, CHANGE);
-
-    Wire.begin();
-
-    // TFT Backlight
+    // Init LCD
+    Arduino_DataBus *bus = new Arduino_HWSPI(D3, D8);
+    gfx = new Arduino_SSD1283A(bus, D6, 2);
+    gfx->begin();
+    gfx->fillScreen(BLACK);
     analogWrite(D0, 128);  // GPIO16
-
-    // Begin SPI before begin TFT
-    SPI.begin();
-
-    tft.begin();
-    tft.setRotation(1);  // portrait
-    tft.fillScreen(TFT_BLACK);
-    // https://github.com/Bodmer/TFT_eSPI/blob/master/examples/Smooth%20Fonts/FLASH_Array/Font_Demo_2_Array/Font_Demo_2_Array.ino
-    tft.setTextColor(TFT_WHITE, TFT_BLACK);
-    tft.loadFont(iosevka15);
-    tft.setTextDatum(TL_DATUM);
-    tft.setTextPadding(180);
 
     // Use GPIO12 as input consider display does not need MISO,
     // only works after SPI & tft are both started.
     // pinMode(D6, INPUT);
-    button.begin();
+    // button.begin();
 
+    // Init INAxxx via I2C
+    Wire.begin();
     devicesFound = INA.begin(MAXIMUM_AMPS, SHUNT_MICRO_OHM);  // Expected max Amp & shunt resistance
     while (devicesFound == 0)
     {
@@ -66,11 +43,10 @@ void setup()
     Serial.print(F(" - Detected "));
     Serial.print(devicesFound);
     Serial.println(F(" INA devices on the I2C bus"));
-    INA.setBusConversion(1000);             // Maximum conversion time 8.244ms
-    INA.setShuntConversion(1000);           // Maximum conversion time 8.244ms
-    INA.setAveraging(32);                   // Average each reading n-times
+    INA.setBusConversion(8500);             // Maximum conversion time 8.244ms
+    INA.setShuntConversion(8500);           // Maximum conversion time 8.244ms
+    INA.setAveraging(128);                  // Average each reading n-times
     INA.setMode(INA_MODE_CONTINUOUS_BOTH);  // Bus/shunt measured continuously
-    INA.alertOnBusOverVoltage(true, 3500);  // Trigger alert if over 3.5V on bus
 }
 
 void loop()
@@ -102,10 +78,14 @@ void loop()
         shuntMicroVolts = INA.getShuntMicroVolts(i);
         busMicroAmps    = INA.getBusMicroAmps(i);
         busMicroWatts   = INA.getBusMicroWatts(i);
-        // if (busMicroAmps < 0)
-        // {
-        //     busMicroAmps = 0;
-        // }
+
+        // There is a slight skew when there is no power or no load. Partially fix it.
+        // By doing this, the negative current cannot be measured, consider INA219 is not a full bi-direction power measurement IC.
+        if (busMilliVolts == 0 || shuntMicroVolts < 0)
+        {
+            shuntMicroVolts = busMicroAmps = busMicroWatts = 0;
+        }
+
         currentMillis = millis();
         deltaMillis   = currentMillis - lastMillis;
         lastMillis    = currentMillis;
@@ -117,41 +97,59 @@ void loop()
                 busMicroWatts / 1000.0, shuntMicroVolts);
         Serial.print(sprintfBuffer);
 
-        sprintf(sprintfBuffer, "0x%2X %s\n", INA.getDeviceAddress(i), INA.getDeviceName(i));
-        tft.setTextColor(TFT_WHITE, TFT_BLACK);
-        tft.drawString(sprintfBuffer, 0, 0);
-        sprintf(sprintfBuffer, "%8.3fV %8.3fmA\n", busMilliVolts / 1000.0, busMicroAmps / 1000.0);
-        tft.setTextColor(TFT_RED, TFT_BLACK);
-        tft.drawString(sprintfBuffer, 0, 16);
-        // sprintf(sprintfBuffer, "Shunt = %9.5fmV\n", shuntMicroVolts / 1000.0);
-        sprintf(sprintfBuffer, "%8.2fmW %8.2fmW\n", busMicroWatts / 1000.0,
-                int64_t(busMilliVolts) * int64_t(busMicroAmps) / 1000000.0);
-        tft.setTextColor(TFT_GREEN, TFT_BLACK);
-        tft.drawString(sprintfBuffer, 0, 32);
+        gfx->setCursor(0, 0);
+        gfx->fillRect(0, 0, 80, 10, BLACK);
+        gfx->setTextColor(RED);
+        gfx->printf("0x%2X %s\n", INA.getDeviceAddress(i), INA.getDeviceName(i));
+
+        gfx->setCursor(0, 16);
+        gfx->fillRect(48, 16, 84, 10, BLACK);
+        gfx->setTextColor(YELLOW);
+        gfx->printf("VOL.    %9.3f V", busMilliVolts / 1000.0);
+
+        gfx->setCursor(0, 32);
+        gfx->fillRect(48, 32, 84, 10, BLACK);
+        gfx->setTextColor(RED);
+        gfx->printf("CURRENT %9.3f mA", busMicroAmps / 1000.0);
+        
+        gfx->setCursor(0, 48);
+        gfx->fillRect(48, 48, 84, 10, BLACK);
+        gfx->setTextColor(GREEN);
+        gfx->printf("SHUNT   %9.3f mV", shuntMicroVolts / 1000.0);
+
+        gfx->setCursor(0, 64);
+        gfx->fillRect(48, 64, 84, 10, BLACK);
+        gfx->setTextColor(CYAN);
+        gfx->printf("POWER   %9.3f mW", busMicroWatts / 1000.0);
+
         // 1mWh = 1000uW x 3600 x 1000ms = 3.6 x 10^9uWh
-        sprintf(sprintfBuffer, "%8.3fmWh %3dm%04.1fs\n", microWattMills / 3600000000.0,
-                (currentMillis - startMillis) / 60000, (currentMillis - startMillis) % 60000 / 1000.0);
-        tft.setTextColor(TFT_YELLOW, TFT_BLACK);
-        tft.drawString(sprintfBuffer, 0, 48);
-        sprintf(sprintfBuffer, "%9.2fohm\n", float(busMilliVolts) / busMicroAmps * 1000.0);
-        tft.setTextColor(TFT_YELLOW, TFT_BLACK);
-        tft.drawString(sprintfBuffer, 0, 64);
+        gfx->setCursor(0, 80);
+        gfx->fillRect(48, 80, 84, 10, BLACK);
+        gfx->setTextColor(MAGENTA);
+        gfx->printf("TOTAL   %9.3f mWh", microWattMills / 3600000000.0);
+
+        gfx->setCursor(0, 96);
+        gfx->fillRect(48, 96, 84, 10, BLACK);
+        gfx->setTextColor(ORANGE);
+        if (busMicroAmps == 0)
+        {
+            gfx->print("R               - ohm\n");
+        }
+        else 
+        {
+            gfx->printf("R       %9.3f ohm\n", float(busMilliVolts) / busMicroAmps * 1000.0);
+        }
+ 
+        gfx->setCursor(0, 112);
+        gfx->fillRect(30, 112, 102, 10, BLACK);
+        gfx->setTextColor(WHITE);
+        gfx->printf("TIME %4d m %5.2f s\n", (currentMillis - startMillis) / 60000, (currentMillis - startMillis) % 60000 / 1000.0);
+
     }  // for-next each INA device loop
-    // Serial.println();
-    // Serial.print(F("Loop iteration "));
-    // Serial.print(++loopCounter);
-    // Serial.print(F("\n\n"));
 
-    // sprintf(sprintfBuffer, "Loop = %d\n", loopCounter);
-    // tft.setTextColor(TFT_BLUE, TFT_BLACK);
-    // tft.drawString(sprintfBuffer, 0, 64);
+    // if (button.toggled())
+    // {
+    // }
 
-    if (button.toggled())
-    {
-        tft.fillRect(135, 30, 20, 20, button.read() == Button::PRESSED ? TFT_RED : TFT_BLACK);
-    }
-
-    tft.fillRect(135, 5, 20, 20, alert ? TFT_MAGENTA : TFT_BLACK);
-
-    delay(100);  // Wait 1 seconds before next reading
+    delay(100);  // Wait 100ms before next reading
 }
--- README.md
+++ README.md
@@ -1,23 +1,31 @@
 # Power Monitor
 
-Monitor the power consumption of ESP32 & ESP8266 devices and figure out how to save power. The DMM does not work well in this case as it is slow. And as a hobby, a dedicated power monitor is not within the budget.
+Monitor the power consumption of ESP32 & ESP8266 devices and figure out how to save power. The DMM does not work well in this case as it is slow. And as an amateur hobbyist, a dedicated power monitor is not within the budget.
 
-![Power Monitor](Hardware/Power%20Monitor.png)
+- First try using INA226
+  ![Power Monitor INA226](Hardware/Power%20Monitor.png)
+- Move to INA219
+  ![INA219 on Breadboard](INA219%20on%20Breadboard.webp)
+  ![INA219 with DMMs](INA219%20with%20DMMs.webp)
 
 ## Plan
 
-- [x] Power monitor IC selection - INA226
-  - [x] INA226 full address selection.
-    - [x] Use DPDT to implement 1-to-4 wire selection for both A1 and A0.
-  - [x] VBUS to VIN+ & VIN- selection.
-  - [x] Expose I2C interface.
-  - [x] Shunt resistor
-    - [x] Single shunt resistor.
-    - [ ] ~~2 shunt resistors by a DPDT?~~
-  - [x] Reverse polarity protection.
-    - VBUS voltage - [0, 36] V.
+- [x] Power monitoring IC selection
+  - [x] ~~INA226~~
+    - [x] ~~INA226 full address selection.~~
+      - [x] ~~Use DPDT switches to implement 1-to-4 wire selection for both A1 and A0.~~
+    - [x] ~~VBUS to VIN+ & VIN- selection.~~
+    - [x] ~~Expose I2C interface.~~
+    - [x] ~~Shunt resistor~~
+      - [x] ~~Single shunt resistor.~~
+      - [ ] ~~2 shunt resistors by a DPDT?~~
+    - [x] ~~Reverse polarity protection.~~
+      - ~~VBUS voltage - [0, 36] V.~~
+  - [x] INA219
 - [x] MCU selection - ESP8266
-- [x] Screen selection - 1.47" 135x240 TFT LCD
+- [x] LCD selection
+  - [x] 1.47" 135x240 TFT LCD
+  - [x] 1.6" 132x132 SSD1283A
 - [x] PCB design
   - [x] Business card size.
   - [x] Connectors
@@ -27,8 +35,11 @@ Monitor the power consumption of ESP32 & ESP8266 devices and figure out how to s
 
 ## Progress
 
-- [x] Test INA226.
+- [x] ~~Test INA226.~~
   - Got a CJMCU board with defect, the current measurement shows -16mA without any load, tried to fix it by code, it is not linear :(. The voltage measurement and the alert functions are working.
+  - I also purchased ICs, but probably got defunct ones.
+- [x] Test INA219.
+  - [x] Works well on breadboard.
 - [x] Test Si4909 reverse polarity protection.
 - [x] PCB fabrication.
 - [x] Ordering parts.
